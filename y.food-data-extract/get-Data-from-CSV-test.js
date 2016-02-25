16a58a0fc30edf3c9c3dc2e71cb8000cf541e655
quanbinn <quanbinn@126.com>
2016-02-25 14:55:27 +0800

    add bat-windows

--- y.food-data-extract/get-Data-from-CSV-test.js
+++ y.food-data-extract/get-Data-from-CSV-test.js
@@ -1,11 +1,11 @@
 fs = require('fs')
 
 var food = {};                                                               // create a empty food
-var dataIndex = 2;                                                           // the position of the nutrition fact needed
+var dataIndex = 5;                                                           // the position of the nutrition fact needed
 
-//var name = "Nutrient data for:.*"
+var name = "Nutrient data for:.*"
+var unit = "Nutrient,Unit,.*"
 var calory = "Energy,kcal,.*"
-//var unit = "Nutrient,Unit,.*"
 var totalFat = "Total lipid.*"
 var saturatedFat = "Fatty acids, total saturated,.*"
 var cholesterol = "Cholesterol,.*"
@@ -16,11 +16,11 @@ fs.readFile('egg.csv', 'utf8', function (err,data) {
   var processedFoodStringHaveQuote = data.toString();                        // change data to the string
 	var processedFoodString = processedFoodStringHaveQuote.replace(/"/g, '');  // remove all " 
 
-	//food.name = getNutritionData(processedFoodString, name, dataIndex);
+	food.name = getFoodName(processedFoodString, name) 
+	food.unit = getNutritionData(processedFoodString, unit, dataIndex);
 	food.calory = getNutritionData(processedFoodString, calory, dataIndex);
-	//food.unit = getNutritionData(processedFoodString, unit, dataIndex);
 	food.totalFat = getNutritionData(processedFoodString, totalFat, dataIndex);
-	food.saturatedFat = getNutritionData(processedFoodString, saturatedFat, dataIndex);
+	food.saturatedFat = getNutritionData(processedFoodString, saturatedFat, dataIndex + 1);
 	food.cholesterol = getNutritionData(processedFoodString, cholesterol, dataIndex);
 
 	function getNutritionData (processedFoodString, matchString, dataIndex) {
@@ -30,6 +30,18 @@ fs.readFile('egg.csv', 'utf8', function (err,data) {
 		var nutritionData = firstElement.split(',')[dataIndex];            // get the nutrition data in the assigned index
 		return nutritionData;                                              // return the nutrition data
 	};
+
+		function getFoodName (processedFoodString, matchString) {
+		var re = new RegExp(matchString, "g");                             // create a RegExp instance - "re"
+		var matchStringResult = processedFoodString.match(re, "regex");    // get the result of regular expression execution - "[]"
+		var firstElement = matchStringResult[0];                           // get the first element of the array
+
+		var str = firstElement.split(", ");
+		var foodName = str[1]+ "," + str[2];
+
+		return foodName;                                             
+	};
+
 
 	console.log("binxia is coming");
 	console.log(food);
