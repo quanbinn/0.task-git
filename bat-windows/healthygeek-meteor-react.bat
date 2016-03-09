7041450230f74f0fe28922aa191287a371947892
quanbinn <quanbinn@126.com>
2016-03-09 15:06:46 +0800

    add healthygeek-meteor-react.bat

---
+++ bat-windows/healthygeek-meteor-react.bat
@@ -0,0 +1,7 @@
+START "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://github.com/quanbinn/healthygeek"
+
+START "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://www.meteor.com/tutorials/react/collections"
+
+START "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://github.com/eventedmind/feed-render-a-collection-with-meteor-and-react/blob/master/app/app.jsx"
+
+START "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://www.eventedmind.com/items/render-a-collection-with-meteor-and-react-208aea9f"
